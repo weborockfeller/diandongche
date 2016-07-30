e35f2e331191a4205eb5430d3962aeca22f35af3
weborockfeller <weborockfeller@sina.cn>
2016-07-30 11:32:50 +0800

    footer and findcar

--- js/findCarCtrl.js
+++ js/findCarCtrl.js
@@ -68,7 +68,15 @@ define(["footApp","jquery"],function (app,$) {
 		};
 		json("./data/electrocar.json");
 
-
+	//设置footer样式
+		$("footer .s1").attr("src", "./images/61_01.png");
+		$("footer .w1").css("color", "#8f8f8f");
+		$("footer .s2").attr("src", "./images/62_02.png");
+		$("footer .w2").css("color", "#069cc4");
+		$("footer .s3").attr("src", "./images/61_03.png");
+		$("footer .w3").css("color", "#8f8f8f");
+		$("footer .s4").attr("src", "./images/61_04.png");
+		$("footer .w4").css("color", "#8f8f8f");
 
 
 	});
--- js/me.js
+++ js/me.js
@@ -36,6 +36,15 @@ define(["footApp","jquery"],function(app,$){
 			$("#yijian h3 img").click(function () {
 				$(this).parents("section").hide();
 			})
+		//设置footer样式
+		$("footer .s1").attr("src", "./images/61_01.png");
+		$("footer .w1").css("color", "#8f8f8f");
+		$("footer .s2").attr("src", "./images/61_02.png");
+		$("footer .w2").css("color", "#8f8f8f");
+		$("footer .s3").attr("src", "./images/61_03.png");
+		$("footer .w3").css("color", "#8f8f8f");
+		$("footer .s4").attr("src", "./images/62_04.png");
+		$("footer .w4").css("color", "#069cc4");
 		});
 	});
 
