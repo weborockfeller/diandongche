00eaeff9812b25e883fce92c9fb4d6ad4f5ed894
genglewei <18639677270@163.com>
2016-07-30 11:12:52 +0800

    1

--- js/findCarCtrl.js
+++ js/findCarCtrl.js
@@ -1,39 +1,75 @@
-define(["footApp", "jquery"], function(app, $) {
-	app.controller('findCarCtrl', function($scope, $http) {
-		$http.get("./data/electrocar.json")
-				.success(function(response) {
+define(["footApp","jquery"],function (app,$) {
+	app.controller("findCarCtrl",function ($scope,$http) {
+		var lis = $(".brand ul li a");
+		// alert(lis.length)
+	    var pinpai = $("#pinpai");
+	    var xuanze = $("#xuanze");
+	    var car_money = $("#car_money");
+	    var da = $("#da");
+	    var price = $("#price");
+	    var car_money_ul = $("#car_money_ul li");
 
-					$scope.electrocar = response.electrocar;
+	    //ajax数据切换
+	    var navA = $("nav a");
 
-				});
-		$("nav>a").click(function() {
-			var data = "electrocar";
-			if($(this).html() == "电动车") {
-				data = "electrocar";
-			} else if($(this).html() == "电动汽车") {
-				data = "electromobile";
-			} else {
-				data = "electroCyclo";
-			}
-			console.log(data);
-			$http.get("./data/" + data + ".json")
-				.success(function(response) {
 
-					$scope.electrocar = response.electrocar;
+	    //选择 品牌隐藏
+	   	lis[1].onclick = function (e) {
+	   		e.preventDefault();
+	   		pinpai.hide();
+	   		xuanze.show();
+	   	};
+	   	lis[0].onclick = function (e) {
+	   		e.preventDefault();
+	   		xuanze.hide();
+	   		pinpai.show();
+	   		// car_money.show();
+	   	};
+	   	//显示
+	   	price[0].onclick = function (e) {
+	   		da.css("transform","translate(-4.31rem)");
+	   		da.css("transition","all 1s")
+	   	};
 
-				});
+	   	//收回
+	   	car_money_ul.click(function () {
+	   		da.css("transform","translate(0rem)");
+	   		da.css("transition","all 1s");
+	   		car_money_ul.css("background","#fff");
+	   		$(this).css("background","red");
+	   		price.html($(this).html())
+	   	});
 
-		});
+	  
 		
-	//设置footer样式
-		$("footer .s1").attr("src", "./images/61_01.png");
-		$("footer .w1").css("color", "#8f8f8f");
-		$("footer .s2").attr("src", "./images/62_02.png");
-		$("footer .w2").css("color", "#069cc4");
-		$("footer .s3").attr("src", "./images/61_03.png");
-		$("footer .w3").css("color", "#8f8f8f");
-		$("footer .s4").attr("src", "./images/61_04.png");
-		$("footer .w4").css("color", "#8f8f8f");
-	});
 
-});
+		//后台数据
+		// $http.get("./data/electrocar.json")
+		// .success(function (data) {
+		// 	// console.log(data)
+		// 	// console.log(data.electrocar);
+		// 	$scope.arr = data.electrocar;
+		// 	// $scope.message = data.electrocar.message;
+		// });
+
+		var array = ["./data/electrocar.json","./data/electroCyclo.json","./data/electromobile.json"];
+		navA.click(function (e) {
+			json(array[$(this).index()]);
+			//阻止事件默认行为
+			return false;
+		});
+
+		//封装起来
+		function json (url) {
+			$http.get(url)
+			.success(function (data) {
+				$scope.arr = data.electrocar;
+			})
+		};
+		json("./data/electrocar.json");
+
+
+
+
+	});
+})
--- js/me.js
+++ js/me.js
@@ -1,6 +1,7 @@
 define(["footApp","jquery"],function(app,$){
 		app.controller('meCtrl', function($scope) {
-			$("nav p").click(function () {
+			$("#nav p").click(function () {
+				// alert(111);
 				$("#denglu").show();
 			});
 		
@@ -35,15 +36,6 @@ define(["footApp","jquery"],function(app,$){
 			$("#yijian h3 img").click(function () {
 				$(this).parents("section").hide();
 			})
-		//设置footer样式
-		$("footer .s1").attr("src", "./images/61_01.png");
-		$("footer .w1").css("color", "#8f8f8f");
-		$("footer .s2").attr("src", "./images/61_02.png");
-		$("footer .w2").css("color", "#8f8f8f");
-		$("footer .s3").attr("src", "./images/61_03.png");
-		$("footer .w3").css("color", "#8f8f8f");
-		$("footer .s4").attr("src", "./images/62_04.png");
-		$("footer .w4").css("color", "#069cc4");
 		});
 	});
 
